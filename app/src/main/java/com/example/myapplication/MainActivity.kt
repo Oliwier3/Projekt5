9f455165b8160a3b202ff45ad123025802c46336
IETI <58682773+marcinkolacz@users.noreply.github.com>
2023-04-03 15:18:32 +0200

    3 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore.Audio.Radio
+import android.widget.CheckBox
 import android.widget.RadioButton
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,9 @@ class MainActivity : AppCompatActivity() {
 
         val radio1 = findViewById<RadioButton>(R.id.radio1)
         val radio2 = findViewById<RadioButton>(R.id.radio2)
-
+        val checkbox1 = findViewById<CheckBox>(R.id.check1)
+        radio1.setOnCheckedChangeListener { compoundButton, b ->
+            checkbox1.isVisible
+        }
     }
 }
