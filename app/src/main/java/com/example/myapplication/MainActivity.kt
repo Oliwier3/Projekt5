6d749cc0446bcbe335b83bcd5844cbfa14a9c943
IETI <58682773+marcinkolacz@users.noreply.github.com>
2023-04-03 15:05:40 +0200

    2 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val radio1 = findViewById<RadioButton>(R.id.radio1)
+        val radio2 = findViewById<RadioButton>(R.id.radio2)
+
     }
 }
