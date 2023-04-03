ac24d1fbf9d78c01a593ce68575dd30847046b84
IETI <58682773+marcinkolacz@users.noreply.github.com>
2023-04-03 15:43:20 +0200

    6 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,9 +14,28 @@ class MainActivity : AppCompatActivity() {
 
         val radio1 = findViewById<RadioButton>(R.id.radio1)
         val radio2 = findViewById<RadioButton>(R.id.radio2)
+        val checkbox = findViewById<CheckBox>(R.id.check)
         val checkbox1 = findViewById<CheckBox>(R.id.check1)
+        val checkbox2 = findViewById<CheckBox>(R.id.check2)
+        val checkbox3 = findViewById<CheckBox>(R.id.check3)
+        val checkbox4 = findViewById<CheckBox>(R.id.check4)
+        val checkbox5 = findViewById<CheckBox>(R.id.check5)
+        val checkbox6 = findViewById<CheckBox>(R.id.check6)
+        val checkbox7 = findViewById<CheckBox>(R.id.check7)
+        val checkbox8 = findViewById<CheckBox>(R.id.check8)
+        val checkbox9 = findViewById<CheckBox>(R.id.check1)
+
         radio1.setOnCheckedChangeListener { compoundButton, b ->
-            checkbox1.isVisible
+            checkbox.isVisible = true
+            checkbox1.isVisible = true
+            checkbox2.isVisible = true
+            checkbox3.isVisible = true
+            checkbox4.isVisible = true
+            checkbox5.isVisible = true
+            checkbox6.isVisible = true
+            checkbox7.isVisible = true
+            checkbox8.isVisible = true
+            checkbox9.isVisible = true
         }
     }
 }
