6b8b7ffda7a0f4ca9cdb3501327f5c4e1f1da665
Oliwier3 <oliwier.krauza@gmail.com>
2023-04-16 18:58:39 +0200

    29 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -41,45 +41,84 @@ class MainActivity : AppCompatActivity() {
             checkbox0.isVisible = true
             chip0.isVisible = false
             checkbox1.isVisible = true
+            chip1.isVisible = false
             checkbox2.isVisible = true
+            chip2.isVisible = false
             checkbox3.isVisible = true
+            chip3.isVisible = false
             checkbox4.isVisible = true
+            chip4.isVisible = false
             checkbox5.isVisible = true
+            chip5.isVisible = false
             checkbox6.isVisible = true
+            chip6.isVisible = false
             checkbox7.isVisible = true
+            chip7.isVisible = false
             checkbox8.isVisible = true
+            chip8.isVisible = false
             checkbox9.isVisible = true
+            chip9.isVisible = false
         }
 
         radio1.setOnCheckedChangeListener { compoundButton, b ->
             if (checkbox0.isChecked == false)
                 checkbox0.isVisible = false
+            else
+                chip0.isVisible=true
+                checkbox0.isVisible = false
 
             if (checkbox1.isChecked == false)
+                checkbox1.isVisible = false
+            else
+                chip1.isVisible=true
                 checkbox1.isVisible = false
 
             if (checkbox2.isChecked == false)
                 checkbox2.isVisible = false
+            else
+                chip2.isVisible=true
+                checkbox2.isVisible = false
 
             if (checkbox3.isChecked == false)
+                checkbox3.isVisible = false
+            else
+                chip3.isVisible=true
                 checkbox3.isVisible = false
 
             if (checkbox4.isChecked == false)
                 checkbox4.isVisible = false
+            else
+                chip4.isVisible=true
+                checkbox4.isVisible = false
 
             if (checkbox5.isChecked == false)
+                checkbox5.isVisible = false
+            else
+                chip5.isVisible=true
                 checkbox5.isVisible = false
 
             if (checkbox6.isChecked == false)
                 checkbox6.isVisible = false
+            else
+                chip6.isVisible=true
+                checkbox6.isVisible = false
 
             if (checkbox7.isChecked == false)
+                checkbox7.isVisible = false
+            else
+                chip7.isVisible=true
                 checkbox7.isVisible = false
 
             if (checkbox8.isChecked == false)
                 checkbox8.isVisible = false
+            else
+                chip8.isVisible=true
+                checkbox8.isVisible = false
 
             if (checkbox9.isChecked == false)
+                checkbox9.isVisible = false
+            else
+                chip9.isVisible=true
                 checkbox9.isVisible = false
 
         }
